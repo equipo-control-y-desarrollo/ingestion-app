3ef179b64a4b913037561cc1ba6520cd2cf88749
unknown <germandavidvargas@hotmail.com>
2022-11-04 21:44:50 -0500

    Flujo de caja routes created

--- src/controllers/flujo_caja.ts
+++ src/controllers/flujo_caja.ts
@@ -1,1 +1,116 @@
 //Flujo caja
+
+import { PrismaClient } from "@prisma/client";
+import { Request, Response, NextFunction } from "express";
+import { createError } from "../utils/errors";
+
+const prisma = new PrismaClient();
+
+export const get_flujo_caja = async ( req: Request, res: Response, next: NextFunction ) => {
+    try {
+        const { id } = req.params;
+        const flujo_caja = await prisma.flujo_caja.findUnique({
+            where: { id: +id },
+        });
+        if ( !flujo_caja ) {
+            return next( createError( 'Flujo caja not found', 404 ) );
+        }
+        res.status( 200 ).json( flujo_caja );
+    }catch(error){
+        next(error)
+    }
+};
+
+export const get_flujo_caja_by_empresa = async ( req: Request, res: Response, next: NextFunction ) => {
+    try {
+        const { empresa_id } = req.params;
+        const empresa = await prisma.empresa.findUnique({
+            where: { id: +empresa_id },
+        });
+        if ( !empresa ) {
+            return next( createError( 'Empresa not found', 404 ) );
+        }
+
+        const flujo_caja = await prisma.flujo_caja.findMany({
+            where: { empresa_id: +empresa_id },
+        });
+        res.status( 200 ).json( flujo_caja );
+    }catch(error){
+        next(error)
+    }
+};
+
+export const create_flujo_caja = async ( req: Request, res: Response, next: NextFunction ) => {
+    try {
+        const {
+            fecha,
+            empresa_id
+        } = req.body;
+
+        const empresa = await prisma.empresa.findUnique({
+            where: { id: +empresa_id },
+        });
+
+        if(!empresa){
+            return next( createError( 'Empresa not found', 404 ) );
+        }
+
+        const flujo_caja = await prisma.flujo_caja.create({
+            data: {
+                fecha: fecha,
+                empresa_id: empresa_id
+            },
+        });
+        res.status( 200 ).json( flujo_caja );
+    }catch(error){
+        next(error);
+    }
+};
+
+export const update_flujo_caja = async ( req: Request, res: Response, next: NextFunction ) => {
+    try {
+        const { id } = req.params;
+        const {
+            fecha,
+            empresa_id
+        } = req.body;
+
+        if(empresa_id){
+            const empresa = await prisma.empresa.findUnique({
+                where: { id: +empresa_id },
+            });
+    
+            if(!empresa){
+                return next( createError( 'Empresa not found', 404 ) );
+            }
+        }
+
+        let new_fecha = undefined
+        if(fecha){
+            new_fecha = new Date(fecha);
+        }
+
+        const flujo_caja = await prisma.flujo_caja.update({
+            where: { id: +id },
+            data: {
+                fecha: new_fecha,
+                empresa_id: empresa_id || undefined
+            },
+        });
+        res.status( 200 ).json( flujo_caja );
+    }catch(error){
+        next(error);
+    }
+};
+
+export const delete_flujo_caja = async ( req: Request, res: Response, next: NextFunction ) => {
+    try {
+        const { id } = req.params;
+        const flujo_caja = await prisma.flujo_caja.delete({
+            where: { id: +id },
+        });
+        res.status( 200 ).json( flujo_caja );
+    }catch(error){
+        next(error);
+    }
+};
--- src/routes/flujo_caja.ts
+++ src/routes/flujo_caja.ts
@@ -1,1 +1,23 @@
 //Flujo caja
+
+import { Router } from 'express';
+
+import {
+    get_flujo_caja,
+    create_flujo_caja,
+    get_flujo_cajas,
+    get_flujo_cajas_by_empresa,
+    update_flujo_caja,
+    delete_flujo_caja,
+} from "../controllers/flujo_caja";
+
+const router = Router();
+
+router.get('/empresa/:empresa_id', get_flujo_cajas_by_empresa);
+router.get('/', get_flujo_cajas);
+router.get('/:id', get_flujo_caja);
+router.post('/', create_flujo_caja);
+router.put('/:id', update_flujo_caja);
+router.delete('/:id', delete_flujo_caja);
+
+export default router;
