3f57f97f29e70417f597d9bc83881d9177fe2066
unknown <germandavidvargas@hotmail.com>
2022-11-05 18:45:44 -0500

    Cuadro ventas routes created

--- src/controllers/ventas.ts
+++ src/controllers/ventas.ts
@@ -134,3 +134,176 @@ export const delete_venta = async (req: Request, res: Response, next: NextFuncti
         next(error);
     }
 };
+
+//Cuadros ventas
+export const get_cuadros_ventas = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const cuadro_ventas = await prisma.cuadro_ventas.findMany();
+        res.status(200).json(cuadro_ventas);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const get_cuadro_venta = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const cuadro_venta = await prisma.cuadro_ventas.findUnique({
+            where: { id: +id },
+        });
+        if (!cuadro_venta) {
+            return next(createError('Cuadro venta not found', 404));
+        }
+        res.status(200).json(cuadro_venta);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const get_cuadros_ventas_by_empresa = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { empresa_id } = req.params;
+        const empresa = await prisma.empresa.findUnique({
+            where: { id: +empresa_id },
+        });
+        if (!empresa) {
+            return next(createError('Empresa not found', 404));
+        }
+
+        const cuadro_ventas = await prisma.cuadro_ventas.findMany({
+            where: { empresa_id: +empresa_id },
+        });
+        res.status(200).json(cuadro_ventas);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const create_cuadro_venta = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {
+            empresa_id,
+            ventas_mañana,
+            ventas_tarde,
+            ventas_totales,
+            efectivo,
+            datafono,
+            transferencia,
+            propinas,
+            iva,
+            hipoconsumo,
+            tks,
+            tks_promedio,
+            epayco,
+            ventas_cafe,
+            ventas_bar,
+            ventas_mercado,
+            gastos_caja_menor
+        } = req.body;
+
+        const empresa = await prisma.empresa.findUnique({
+            where: { id: +empresa_id },
+        });
+
+        if (!empresa) {
+            return next(createError('Empresa not found', 404));
+        }
+
+        const cuadro_venta = await prisma.cuadro_ventas.create({
+            data: {
+                empresa_id: empresa_id,
+                ventas_mañana: ventas_mañana,
+                ventas_tarde: ventas_tarde,
+                ventas_totales: ventas_totales,
+                efectivo: efectivo,
+                datafono: datafono,
+                transferencia: transferencia,
+                propinas: propinas,
+                iva: iva,
+                hipoconsumo: hipoconsumo,
+                tks: tks,
+                tks_promedio: tks_promedio,
+                epayco: epayco,
+                ventas_cafe: ventas_cafe,
+                ventas_bar: ventas_bar,
+                ventas_mercado: ventas_mercado,
+                gastos_caja_menor: gastos_caja_menor
+            },
+        });
+        res.status(201).json(cuadro_venta);
+    }catch(error){
+        next(error);
+    }
+};
+
+export const update_cuadro_venta = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const {
+            empresa_id,
+            ventas_mañana,
+            ventas_tarde,
+            ventas_totales,
+            efectivo,
+            datafono,
+            transferencia,
+            propinas,
+            iva,
+            hipoconsumo,
+            tks,
+            tks_promedio,
+            epayco,
+            ventas_cafe,
+            ventas_bar,
+            ventas_mercado,
+            gastos_caja_menor
+        } = req.body;
+
+        if(!empresa_id){
+            const empresa = await prisma.empresa.findUnique({
+                where: { id: +empresa_id },
+            });
+            if (!empresa) {
+                return next(createError('Empresa not found', 404));
+            }
+        }
+
+        const updated_cuadro_venta = await prisma.cuadro_ventas.update({
+            where: { id: +id },
+            data: {
+                empresa_id: empresa_id || undefined,
+                ventas_mañana: ventas_mañana || undefined,
+                ventas_tarde: ventas_tarde || undefined,
+                ventas_totales: ventas_totales || undefined,
+                efectivo: efectivo || undefined,
+                datafono: datafono || undefined,
+                transferencia: transferencia || undefined,
+                propinas: propinas || undefined,
+                iva: iva || undefined,
+                hipoconsumo: hipoconsumo || undefined,
+                tks: tks || undefined,
+                tks_promedio: tks_promedio || undefined,
+                epayco: epayco || undefined,
+                ventas_cafe: ventas_cafe || undefined,
+                ventas_bar: ventas_bar || undefined,
+                ventas_mercado: ventas_mercado || undefined,
+                gastos_caja_menor: gastos_caja_menor || undefined
+            },
+        });
+        res.status(200).json(updated_cuadro_venta);
+    }catch(error){
+        next(error);
+    }
+};
+
+export const delete_cuadro_venta = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const cuadro_venta = await prisma.cuadro_ventas.delete({
+            where: { id: +id },
+        });
+        res.status(200).json(cuadro_venta);
+    } catch (error) {
+        next(error);
+    }
+};
--- src/routes/ventas.ts
+++ src/routes/ventas.ts
@@ -9,6 +9,12 @@ import {
     get_ventas_by_empresa,
     update_venta,
     delete_venta,
+    get_cuadros_ventas,
+    get_cuadro_ventas_by_empresa,
+    get_cuadro_ventas,
+    update_cuadro_ventas,
+    create_cuadro_ventas,
+    delete_cuadro_ventas,
 } from "../controllers/ventas";
 
 const router = Router();
@@ -19,5 +25,13 @@ router.get('/:id', get_venta);
 router.post('/', create_venta);
 router.put('/:id', update_venta);
 router.delete('/:id', delete_venta);
+
+
+router.get('/cuadros/empresa/:empresa_id', get_cuadro_ventas_by_empresa);
+router.get('/cuadros/', get_cuadros_ventas);
+router.get('/cuadros/:id', get_cuadro_ventas);
+router.post('/cuadros/', create_cuadro_ventas);
+router.put('/cuadros/:id', update_cuadro_ventas);
+router.delete('/cuadros/:id', delete_cuadro_ventas);
 
 export default router;
