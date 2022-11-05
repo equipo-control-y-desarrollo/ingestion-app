e5ddc2c1bbe696d1ddb670a3183da6b6c550babf
unknown <germandavidvargas@hotmail.com>
2022-11-05 11:24:46 -0500

    Flujo de caja and Categorias created

--- src/controllers/flujo_caja.ts
+++ src/controllers/flujo_caja.ts
@@ -44,7 +44,8 @@ export const create_flujo_caja = async ( req: Request, res: Response, next: Next
     try {
         const {
             fecha,
-            empresa_id
+            empresa_id,
+            saldo_anterior
         } = req.body;
 
         const empresa = await prisma.empresa.findUnique({
@@ -58,7 +59,8 @@ export const create_flujo_caja = async ( req: Request, res: Response, next: Next
         const flujo_caja = await prisma.flujo_caja.create({
             data: {
                 fecha: fecha,
-                empresa_id: empresa_id
+                empresa_id: empresa_id,
+                saldo_anterior: saldo_anterior
             },
         });
         res.status( 200 ).json( flujo_caja );
@@ -72,7 +74,8 @@ export const update_flujo_caja = async ( req: Request, res: Response, next: Next
         const { id } = req.params;
         const {
             fecha,
-            empresa_id
+            empresa_id,
+            saldo_anterior
         } = req.body;
 
         if(empresa_id){
@@ -94,7 +97,8 @@ export const update_flujo_caja = async ( req: Request, res: Response, next: Next
             where: { id: +id },
             data: {
                 fecha: new_fecha,
-                empresa_id: empresa_id || undefined
+                empresa_id: empresa_id || undefined,
+                saldo_anterior: saldo_anterior || undefined
             },
         });
         res.status( 200 ).json( flujo_caja );
@@ -110,6 +114,128 @@ export const delete_flujo_caja = async ( req: Request, res: Response, next: Next
             where: { id: +id },
         });
         res.status( 200 ).json( flujo_caja );
+    }catch(error){
+        next(error);
+    }
+};
+
+//Categorias 
+
+export const get_categoria = async ( req: Request, res: Response, next: NextFunction ) => {
+    try {
+        const { id } = req.params;
+        const categoria = await prisma.categoria.findUnique({
+            where: { id: +id },
+        });
+        if ( !categoria ) {
+            return next( createError( 'Categoria not found', 404 ) );
+        }
+        res.status( 200 ).json( categoria );
+    }catch(error){
+        next(error)
+    }
+};
+
+export const get_categoria_by_empresa = async ( req: Request, res: Response, next: NextFunction ) => {
+    try {
+        const { empresa_id } = req.params;
+        const empresa = await prisma.empresa.findUnique({
+            where: { id: +empresa_id },
+        });
+        if ( !empresa ) {
+            return next( createError( 'Empresa not found', 404 ) );
+        }
+
+        const categoria = await prisma.categoria.findMany({
+            where: { empresa_id: +empresa_id },
+        });
+        res.status( 200 ).json( categoria );
+    }catch(error){
+        next(error)
+    }
+};
+
+export const create_categoria = async ( req: Request, res: Response, next: NextFunction ) => {
+    try {
+        const {
+            descripcion,
+            efectivo,
+            datafono,
+            transferencia,
+            gastos,
+            flujo_caja_id
+        } = req.body
+
+        const flujo_caja = await prisma.flujo_caja.findUnique({
+            where: { id: +flujo_caja_id },
+        });
+
+        if(!flujo_caja){
+            return next( createError( 'Flujo caja not found', 404 ) );
+        }
+
+        const categoria = await prisma.categoria.create({
+            data: {
+                descripcion: descripcion,
+                efectivo: efectivo || undefined,
+                datafono: datafono || undefined,
+                transferencia: transferencia || undefined,
+                gastos: gastos || undefined,
+                flujo_caja_id: flujo_caja_id
+            },
+        });
+        res.status( 200 ).json( categoria );
+    }catch(error){
+        next(error);
+    }
+};
+
+export const update_categoria = async ( req: Request, res: Response, next: NextFunction ) => {
+    try {
+        const { id } = req.params;
+        const {
+            descripcion,
+            efectivo,
+            datafono,
+            transferencia,
+            gastos,
+            flujo_caja_id
+        } = req.body;
+
+        if(flujo_caja_id){
+            const flujo_caja = await prisma.flujo_caja.findUnique({
+                where: { id: +flujo_caja_id },
+            });
+    
+            if(!flujo_caja){
+                return next( createError( 'Flujo caja not found', 404 ) );
+            }
+        }
+
+        const categoria = await prisma.categoria.update({
+            where: { id: +id },
+            data: {
+                descripcion: descripcion || undefined,
+                efectivo: efectivo || undefined,
+                datafono: datafono || undefined,
+                transferencia: transferencia || undefined,
+                gastos: gastos || undefined,
+                flujo_caja_id: flujo_caja_id || undefined
+            },
+        });
+        res.status( 200 ).json( categoria );
+    }catch(error){
+        next(error);
+    }
+};
+
+export const delete_categoria = async ( req: Request, res: Response, next: NextFunction ) => {
+    try {
+        const { id } = req.params;
+        const categoria = await prisma.categoria.delete({
+            where: { id: +id },
+        });
+        res.status( 200 ).json( categoria );
     }catch(error){
         next(error);
     }
--- src/routes/flujo_caja.ts
+++ src/routes/flujo_caja.ts
@@ -5,19 +5,29 @@ import { Router } from 'express';
 import {
     get_flujo_caja,
     create_flujo_caja,
-    get_flujo_cajas,
-    get_flujo_cajas_by_empresa,
+    get_flujo_caja_by_empresa,
     update_flujo_caja,
     delete_flujo_caja,
+    get_categorias_by_flujo_caja,
+    create_categoria,
+    get_categoria,
+    update_categoria,
+    delete_categoria,
 } from "../controllers/flujo_caja";
 
 const router = Router();
 
-router.get('/empresa/:empresa_id', get_flujo_cajas_by_empresa);
-router.get('/', get_flujo_cajas);
+router.get('/empresa/:empresa_id', get_flujo_caja_by_empresa);
 router.get('/:id', get_flujo_caja);
 router.post('/', create_flujo_caja);
 router.put('/:id', update_flujo_caja);
 router.delete('/:id', delete_flujo_caja);
+
+router.get('/categorias/:flujo_caja_id', get_categorias_by_flujo_caja);
+router.get('/categorias/:id', get_categoria);
+router.post('/categorias', create_categoria);
+router.put('/categorias/:id', update_categoria);
+router.delete('/categorias/:id', delete_categoria);
+
 
 export default router;
