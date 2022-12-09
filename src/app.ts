8744ece83c873fe95a4ead49df90f3ad3f501fd8
Germán Vargas <germandavidvargas@hotmail.com>
2022-12-09 16:50:26 -0500

    cors config started

--- src/app.ts
+++ src/app.ts
@@ -19,12 +19,15 @@ import { verifyToken } from './utils/jwt';
 
 dotenv.config();
 
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL || "*",
+    credentials: true,
+    "Access-Control-Allow-Credentials": true
+}));
 app.use(helmet());
 app.use(cookieParser());
 
