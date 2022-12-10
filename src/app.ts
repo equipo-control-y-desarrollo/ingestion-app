b0e54f602f114b71cd1cd395c6a0601c9b6aec8c
Germán Vargas <germandavidvargas@hotmail.com>
2022-12-10 17:40:31 -0500

    cors error solution

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: "http://localhost:3000",
     credentials: true,
     "Access-Control-Allow-Credentials": true
 }));
