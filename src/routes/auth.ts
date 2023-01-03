263eb05988b218ba36b2c76e14772ad875430561
Germán Vargas <germandavidvargas@hotmail.com>
2023-01-03 12:15:03 -0500

    signup may be used only by admins

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -8,11 +8,12 @@ import {
 } from "../controllers/auth";
 
 import { loginSchema, signupSchema } from '../schemas/auth.schema';
+import { verifyAdmin } from '../utils/jwt';
 import { validate } from '../utils/validation';
 
 const router = Router();
 
 router.post('/login', validate(loginSchema),login);
-router.post('/signup', validate(signupSchema),signup);
+router.post('/signup', [validate(signupSchema), verifyAdmin],signup);
 
 export default router;
